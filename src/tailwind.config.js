c4b9a0a0b7f689e9ad6556a472bee50a1df6c235
wendyhamel <whamel@it4web.nl>
2023-05-31 15:46:17 +0200

    HTML structure and tailwind added.

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Karla','sans-serif']
+		},
+		extend: {
+			colors: {
+				'cyan': 'hsl(179, 62%, 43%)',
+				'yellow': 'hsl(71, 73%, 54%)',
+				'gray-light': 'hsl(204, 43%, 93%)',
+				'gray-blue': 'hsl(218, 22%, 67%)',
+			}
+		}
+	}
+}
