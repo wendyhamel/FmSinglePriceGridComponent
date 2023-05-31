ea8637a1c498739953d66189f4f9e4900dd0fde8
wendyhamel <whamel@it4web.nl>
2023-05-31 16:41:13 +0200

    Desktop styling

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -9,6 +9,9 @@ tailwind.config = {
 				'yellow': 'hsl(71, 73%, 54%)',
 				'gray-light': 'hsl(204, 43%, 93%)',
 				'gray-blue': 'hsl(218, 22%, 67%)',
+			},
+			boxShadow: {
+				'around': '0 15px 15px 4px rgb(0 0 0 / 0.1), 0 10px 10px -6px rgb(0 0 0 / 0.1)'
 			}
 		}
 	}
